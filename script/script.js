52495003eae72ebf4835f35f75a86b1134dc2408
Danil <dg444k@gmail.com>
2023-04-03 23:08:42 +0300

    -add html-skelet

---
+++ script/script.js
@@ -0,0 +1,29 @@
+let myLibrary = [];
+
+function Book(title, 
+              author,
+              pages,
+              genre,
+              statusRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.genre = genre;
+  this.statusRead = statusRead;  
+}
+
+function addBookToLibrary() {
+  let title = ;
+  let author = ;
+  let pages = ;
+  let genre = ;
+  let statusRead = ;
+
+  let newBook = new Book(title, 
+                         author,
+                         pages,
+                         genre,
+                         statusRead);
+                    
+
+}
