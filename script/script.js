68fa56c4a54468c1a04f4e15c203eb7491e89a18
Danil <dg444k@gmail.com>
2023-04-04 02:41:47 +0300

    -add function add cards

--- script/script.js
+++ script/script.js
@@ -4,9 +4,21 @@ const pagesBook = document.getElementById("pages");
 const genreBook = document.getElementById("genre");
 const checkReadBook = document.getElementById("check-read");
 const buttonSubmit = document.querySelector(".btn-submit");
+const buttonAddCard = document.querySelector(".add-big-card");
+const popUp = document.querySelector(".pop-up");
+const parentCards = document.querySelector(".container")
 let myLibrary = [];
 
 buttonSubmit.addEventListener("click", addBookToLibrary);
+buttonAddCard.addEventListener("click", addCard);
+popUp.addEventListener("click", (event) => {
+      event.stopPropagation();
+});
+document.addEventListener("click", (event) => {
+  if (event.currentTarget != popUp) {
+    popUp.style.display = "none";
+  }
+});
 
 function Book(title, author, pages, genre, statusRead) {
   this.title = title;
@@ -25,5 +37,15 @@ function addBookToLibrary(event) {
 
   let newBook = new Book(title, author, pages, genre, statusRead);
   myLibrary.push(newBook);
+  popUp.style.display = "none";
+
+  let newCard = document.createElement("div");
+  parentCards.insertBefore(newCard, parentCards.firstChild);
+  newCard.classList.add('card');
   event.preventDefault();
+}
+
+function addCard(event) {
+    popUp.style.display = "flex";
+    event.stopPropagation();
 }
